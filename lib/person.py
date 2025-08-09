c334065efa79b8b3e49eefe3d83c7a7a03c05c92
creeksonjoseph <charanajoseph@gmail.com>
2025-08-09 16:02:52 +0300

    h

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+   
+   def walk(self):
+         print("The person is walking.")
 
-    #Instance method definition
-    pass
+    
+   def talk(self):
+      print("Hello World!")
+
+
